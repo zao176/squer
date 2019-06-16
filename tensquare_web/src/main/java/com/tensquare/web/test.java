10c80c8720bef2eecb6de4f4c68cbb8b41c36706
zao <799548225@qq.com>
2019-06-16 13:46:11 +0800

    打印哪里222222222

--- tensquare_web/src/main/java/com/tensquare/web/test.java
+++ tensquare_web/src/main/java/com/tensquare/web/test.java
@@ -8,5 +8,8 @@ package com.tensquare.web;
 public class test {
     public static void main(String[] args) {
         System.out.println(1111);
+        System.out.println("你好");
+
+        System.out.println("2222");
     }
 }
