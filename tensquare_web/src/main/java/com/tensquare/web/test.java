03abfafca591403aa0869b154be617af82b9982c
zao <799548225@qq.com>
2019-06-16 13:52:54 +0800

    清空

--- tensquare_web/src/main/java/com/tensquare/web/test.java
+++ tensquare_web/src/main/java/com/tensquare/web/test.java
@@ -7,9 +7,6 @@ package com.tensquare.web;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println(1111);
-        System.out.println("你好");
 
-        System.out.println("2222");
     }
 }
