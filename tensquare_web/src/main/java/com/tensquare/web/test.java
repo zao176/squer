51411469ba290e94cde9f67bbb5a1d7c3ac5810c
zao <799548225@qq.com>
2019-06-16 13:23:40 +0800

    新建了test.java

---
+++ tensquare_web/src/main/java/com/tensquare/web/test.java
@@ -0,0 +1,12 @@
+package com.tensquare.web;
+
+/**
+ * @Auther: ZAO
+ * @Date: 2019/6/16 13:22
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
