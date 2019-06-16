dcfadb6efef13a5ff924d5b299de5326e8155f51
zao <799548225@qq.com>
2019-06-16 13:54:40 +0800

    打印三号

--- tensquare_web/src/main/java/com/tensquare/web/test.java
+++ tensquare_web/src/main/java/com/tensquare/web/test.java
@@ -9,7 +9,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println(1111);
         System.out.println("你好");
-
         System.out.println("2222");
     }
 }
